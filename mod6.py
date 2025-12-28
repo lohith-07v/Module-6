7cd1b91965c8def78242581ca173a57418462cdc
lohith-07v <lohit1511@gmail.com>
2025-12-28 14:34:31 +0530

    done

---
+++ mod6.py
@@ -0,0 +1,15 @@
+class Beans(): 
+     def type(self): 
+       print("Vegetable") 
+     def color(self):
+       print("Green") 
+class Mango(): 
+     def type(self): 
+       print("Fruit") 
+     def color(self): 
+       print("Yellow")
+obj_beans = Beans() 
+obj_mango = Mango()
+for func in (obj_beans,obj_mango): 
+    func.type()
+    func.color()
